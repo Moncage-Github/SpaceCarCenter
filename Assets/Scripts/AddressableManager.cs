1930230eddc3cc30a914e47f2b7b0f493ce07f91
oTangEo <90256741+JhoHwan@users.noreply.github.com>
2025-01-11 15:10:42 +0900

    Reapply "1/11"
    This reverts commit a4a22ea21a2159c883fd32657a5cb63222517f8a.

--- Assets/Scripts/AddressableManager.cs
+++ Assets/Scripts/AddressableManager.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class AddressableManager : MonoBehaviour
+public abstract class AddressableManager : MonoBehaviour
 {
     [SerializeField] protected AssetLabelReference AssetLabel;
 
-    private SerializableDictionary<string, GameObject> _prefabs = new();
     private AsyncOperationHandle<IList<Object>> _handle;
-
-    public SerializableDictionary<string, GameObject> Prefabs
-    {
-        get => _prefabs;
-        protected set => _prefabs = value;
-    }
 
     public void StartLoadAsset()
     {
@@ -33,23 +26,14 @@ public class AddressableManager : MonoBehaviour
     private IEnumerator LoadAssetAsync()
     {
         _handle = Addressables.LoadAssetsAsync<Object>(AssetLabel, null);
-        _handle.Completed += CompleteLoadAsset;
 
         while (!_handle.IsDone)
         {
             yield return null;
         }
+
+        CompleteLoadAsset(_handle);
     }
 
-    public virtual void CompleteLoadAsset(AsyncOperationHandle<IList<Object>> handle)
-    {
-        if (_handle.Status == AsyncOperationStatus.Succeeded)
-        {
-            var result = _handle.Result;
-            foreach (var obj in result)
-            {
-                Prefabs[obj.name] = (GameObject)obj;
-            }
-        }
-    }
+    public abstract void CompleteLoadAsset(AsyncOperationHandle<IList<Object>> handle);
 }
